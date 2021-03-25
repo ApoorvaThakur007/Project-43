455d0e873bfeb85a84172a7e4b22bea451dad84b
ApoorvaThakur007 <74656035+ApoorvaThakur007@users.noreply.github.com>
2021-03-25 19:26:17 +0530

    project

---
+++ sketch.js
@@ -0,0 +1,146 @@
+var hr,min,sec;
+var hrAngle,mnAngle,scAngle;
+
+function setup() {
+  createCanvas(700,700);
+ 
+  angleMode(DEGREES)
+
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("black");
+  fill("white")  
+  textSize(50)
+  text("Tick-Tock",250,100)
+
+  fill("white")  
+  textSize(50)
+  text("Time:"+hr%12+":"+min+":"+sec,225,500)
+
+  translate(350,300)
+  rotate(-90)
+
+  hr = hour()
+  min = minute()
+  sec = second()
+
+  noFill()
+  push()
+  scAngle = map(sec,0,60,0,360)
+  stroke("pink")
+  strokeWeight(3)
+  arc(0,0,300,300,0,scAngle)
+  pop()
+
+  push()
+  mnAngle = map(min,0,60,0,360)
+  stroke("lightBlue")
+  strokeWeight(3)
+  arc(0,0,280,280,0,mnAngle)
+  pop()
+
+  push()
+  hrAngle = map(hr%12,0,60,0,360)
+  stroke("lightGreen")
+  strokeWeight(3)
+  arc(0,0,260,260,0,hrAngle)
+  pop()
+
+  push()
+  rotate(scAngle)
+  stroke("pink")
+  strokeWeight(3)
+  line(0,0,100,0)
+  pop()
+
+  push()
+  rotate(mnAngle)
+  stroke("lightBlue")
+  strokeWeight(3)
+  line(0,0,70,0)
+  pop()
+
+  push()
+  rotate(hrAngle)
+  stroke("lightGreen")
+  strokeWeight(3)
+  line(0,0,50,0)
+  pop()
+
+  stroke("black")
+  line(0,0,0,0)
+
+  
+}
+
+/*function setup() {
+  createCanvas(450,450);
+  angleMode(DEGREES);
+}
+
+function draw() {
+
+  background(0);
+
+  fill(255);
+  textSize(24);
+  textStyle("bold");
+  text(" ~ Tick - Tock Clock ~  :D", 100,400); 
+  text("Blue and Yellow Colour Denotes Second Hand",50,450);
+
+translate(200,200)
+rotate (-90);
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+  
+  strokeWeight(8);
+  stroke("yellow");
+  noFill();
+  let end = map(sc,0,60,0,360);
+  arc(0,0,300,300,0,end);
+  
+  stroke("Pink");
+  let end2 = map(mn,0,60,0,360);
+  arc(0,0,280,280,0,end2);
+  
+  stroke("Light blue");
+  let end3 = map(hr%12,0,12,-0,360);
+  arc(0,0,260,260,0,end3);
+  
+ /* fill(255);
+  textSize(24);
+  textStyle("bold");
+  text("Time : " + end3 + " : " + end2 + " : " + end , 50 , 400);
+//
+  push();
+  rotate(end);
+  stroke("blue");
+  line(0,0,100,0);
+  pop();
+  
+  
+  push();
+  rotate(end2);
+  stroke("red");
+  line(0,0,75,0);
+  pop();
+  
+  push();
+  rotate(end3);
+  stroke("green");
+  line(0,0,50,0);
+  pop();
+  
+  stroke("Black");
+  point (0,0);
+
+  
+
+
+  }
+  */
+  
